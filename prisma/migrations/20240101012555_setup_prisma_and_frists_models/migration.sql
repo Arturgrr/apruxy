dd1d4dfe8764f847f787dc436117abbd82ed7dbe
Artur <ARTURASSISGUERRA@GMAIL.COM>
2024-01-02 03:50:09 -0300

    Fix migrations

--- prisma/migrations/20240101012555_setup_prisma_and_frists_models/migration.sql
+++ prisma/migrations/20240101012555_setup_prisma_and_frists_models/migration.sql
@@ -2,7 +2,6 @@
 CREATE TABLE "User" (
     "id" BIGINT NOT NULL,
     "commandsCounter" INTEGER NOT NULL DEFAULT 0,
-    "fristInteraction" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -11,7 +10,6 @@ CREATE TABLE "User" (
 CREATE TABLE "Guild" (
     "id" BIGINT NOT NULL,
     "commandsCounter" INTEGER NOT NULL DEFAULT 0,
-    "joinedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Guild_pkey" PRIMARY KEY ("id")
 );
